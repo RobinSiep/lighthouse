a74a088c5a3a4c3d20ffc1d8edb15357c5ea3509
Robin Siep <RobinSiep1@gmail.com>
2020-02-11 11:00:45 +0100

    be explicit about UUID type

--- lighthousemaster/models/machine.py
+++ lighthousemaster/models/machine.py
@@ -1,6 +1,7 @@
 import uuid
 
 from sqlalchemy import Column, String
+from sqlalchemy.dialects.postgresql import UUID
 
 from lighthousemaster.db import Base, DBSession as session
 
@@ -8,7 +9,7 @@ from lighthousemaster.db import Base, DBSession as session
 class Machine(Base):
     __tablename__ = 'machine'
 
-    id = Column(String(36), primary_key=True, default=uuid.uuid4)
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     name = Column(String(64), unique=True, nullable=False)
     mac_address = Column(String(17), unique=True, nullable=False)
 
