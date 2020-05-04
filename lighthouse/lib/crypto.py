c60d4e0eddbca2a5152bc43864f5b4fc0d593daa
Robin Siep <RobinSiep1@gmail.com>
2020-05-04 13:52:02 +0200

    hash_str test

--- lighthouse/lib/crypto.py
+++ lighthouse/lib/crypto.py
@@ -4,7 +4,11 @@ import binascii
 from Crypto import Random
 
 
-def hash_str(plaintext, salt=bcrypt.gensalt()):
+def hash_str(plaintext, salt=None):
+    if not salt:
+        # Default salt on runtime instead of compile time to avoid having the
+        # same default salt when calling the method multiple times.
+        salt = bcrypt.gensalt()
     return (bcrypt.hashpw(plaintext.encode('utf-8'), salt).decode('utf-8'),
             salt.decode('utf-8'))
 
---
+++ lighthouse/tests/lib/test_crypto.py
@@ -0,0 +1,27 @@
+from unittest import TestCase
+
+from lighthouse.lib.crypto import hash_str
+
+
+class TestHashStr(TestCase):
+    def test_given_salt(self):
+        expected_hash = '$2b$12$za0avPx11NmhJbk6so4xfOJgCXAp9dJj9SBF7YOl9jKI40M.3KTye'  # noqa
+        salt = '$2b$12$za0avPx11NmhJbk6so4xfO'
+
+        result_hash, result_salt = hash_str("test data", salt.encode('utf-8'))
+
+        self.assertEqual(result_hash, expected_hash)
+        self.assertEqual(result_salt, salt)
+
+    def test_invalid_given_salt(self):
+        with self.assertRaisesRegex(ValueError, "Invalid salt"):
+            hash_str('testing', b'invalid')
+
+    def test_generated_salt(self):
+        result_hash_1, result_salt_1 = hash_str("test data")
+        result_hash_2, result_salt_2 = hash_str("test data")
+        confirmation_hash, _ = hash_str("test data",
+                                        result_salt_1.encode('utf-8'))
+
+        self.assertNotEqual(result_salt_1, result_salt_2)
+        self.assertEqual(result_hash_1, confirmation_hash)
