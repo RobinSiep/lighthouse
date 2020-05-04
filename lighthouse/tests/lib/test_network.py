2b29babbe74eaf7cde820945b4d8578c0302252b
Robin Siep <RobinSiep1@gmail.com>
2020-05-04 12:57:04 +0200

    more network tests and refactoring

--- lighthouse/tests/lib/test_network.py
+++ lighthouse/tests/lib/test_network.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 
-from lighthouse.lib.network import network_addr_to_binary_string
+from lighthouse.lib.network import (
+    network_addr_to_binary_string, get_subnet_size, valid_subnet_mask)
 
 
 class TestNetworkAddrToBinaryString(TestCase):
@@ -9,4 +10,23 @@ class TestNetworkAddrToBinaryString(TestCase):
         self.assertEqual(binary_addr, "11000000101010001011001000101001")
 
     def test_invalid_address(self):
-        pass
+        with self.assertRaises(ValueError):
+            network_addr_to_binary_string("Not a valid address")
+
+
+class TestGetSubnetSize(TestCase):
+    def test_subnet_size(self):
+        subnet_size = get_subnet_size("255.255.255.0")
+        self.assertEqual(subnet_size, 24)
+
+    def test_invalid_address(self):
+        with self.assertRaisesRegex(ValueError, "Not a valid subnet mask"):
+            get_subnet_size("192.168.178.000")
+
+
+class TestValidSubnetMask(TestCase):
+    def test_valid_subnet_mask(self):
+        self.assertTrue(valid_subnet_mask("255.255.255.0"))
+
+    def test_invalid_subnet_mask(self):
+        self.assertFalse(valid_subnet_mask("Not a valid subnet mask"))
