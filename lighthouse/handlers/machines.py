99081335d83a647c363abd30fb0e35665637bf3b
Robin Siep <RobinSiep1@gmail.com>
2020-05-12 10:31:42 +0200

    get_machine method

--- lighthouse/handlers/machines.py
+++ lighthouse/handlers/machines.py
@@ -1,23 +1,29 @@
-from aiohttp import web
+from aiohttp.web import json_response
 
 from lighthouse.app import sio
+from lighthouse.lib.exceptions import JsonHTTPNotFound
 from lighthouse.lib.routes import routes
 from lighthouse.lib.security.decorators import permission_required
 from lighthouse.machine import get_active_machine_on_same_subnet
 from lighthouse.models.machine import get_machine_by_id
 
 
+def get_machine(request):
+    machine = get_machine_by_id(request.match_info['id'])
+    if not machine:
+        raise JsonHTTPNotFound()
+    return machine
+
+
 @routes.post("/machines/{id}/wake")
 @permission_required('wake_on_lan')
 async def send_wake_on_LAN_packet(request):
-    machine = get_machine_by_id(request.match_info['id'])
-    if not machine:
-        return web.json_response(status=404)
-
+    machine = get_machine(request)
     capable_machine = get_active_machine_on_same_subnet(machine)
     if not capable_machine:
-        return web.json_response(
-            "No machine was found capable of waking up the requested machine",
+        return json_response(
+            "No machine was found capable of waking up "
+            "the requested machine",
             status=409
         )
 
@@ -26,18 +32,15 @@ async def send_wake_on_LAN_packet(request):
         machine.mac_address,
         to=capable_machine.sid
     )
-    return web.json_response()
+    return json_response()
 
 
 @routes.post("/machines/{id}/shutdown")
 @permission_required('shutdown')
 async def shutdown(request):
-    machine = get_machine_by_id(request.match_info['id'])
-    if not machine:
-        return web.json_response(status=404)
-
+    machine = get_machine(request)
     await sio.emit('shutdown', to=machine.sid, callback=shutdown_callback)
-    return web.json_response()
+    return json_response()
 
 
 def shutdown_callback(status, error=None):
