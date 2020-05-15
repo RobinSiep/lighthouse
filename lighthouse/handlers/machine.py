c283efd3ab5a00e47776d18532594fe81374472a
Robin Siep <RobinSiep1@gmail.com>
2020-05-15 10:54:01 +0200

    reboot handler working with polling

--- lighthouse/handlers/machine.py
+++ lighthouse/handlers/machine.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from aiohttp.web import json_response
 from aiohttp_session import get_session
 
@@ -21,20 +23,29 @@ def get_machine(request):
 @permission_required('wake_on_lan')
 async def send_wake_on_LAN_packet(request):
     machine = get_machine(request)
-    capable_machine = get_active_machine_on_same_subnet(machine)
-    if not capable_machine:
-        return json_response(
-            "No machine was found capable of waking up "
-            "the requested machine",
-            status=409
-        )
-
-    await sio.emit(
-        'send_wake_on_LAN_packet',
-        machine.mac_address,
-        to=capable_machine.sid
+    await _wake(
+        machine,
+        get_wake_on_LAN_capable_machine(machine)
     )
     return json_response()
+
+
+def get_wake_on_LAN_capable_machine(target_machine):
+    wol_capable_machine = get_active_machine_on_same_subnet(target_machine)
+    if not wol_capable_machine:
+        raise JsonHTTPConflict(
+            "No machine was found capable of waking up "
+            "the requested machine"
+        )
+    return wol_capable_machine
+
+
+async def _wake(target_machine, wol_capable_machine):
+    await sio.emit(
+        'send_wake_on_LAN_packet',
+        target_machine.mac_address,
+        to=wol_capable_machine.sid
+    )
 
 
 @routes.post("/machines/{id}/shutdown")
@@ -46,13 +57,50 @@ async def shutdown(request):
 
     session = await get_session(request)
     sid = session.get('sid')
+    await _shutdown(machine, sid)
 
+    return json_response()
+
+
+async def _shutdown(machine, requesting_sid):
     await sio.emit(
         'shutdown',
         to=machine.sid,
-        callback=lambda status, error: shutdown_callback(status, error, sid)
+        callback=lambda status, error: shutdown_callback(
+            status, error, requesting_sid
+        )
     )
-    return json_response()
+
+
+@routes.post("/machines/{id}/reboot")
+@permission_required('reboot')
+async def reboot(request):
+    machine = get_machine(request)
+    wol_capable_machine = get_wake_on_LAN_capable_machine(machine)
+
+    if get_active_machine(machine.sid) is None:
+        await _wake(machine.sid, wol_capable_machine)
+        return json_response()
+
+    session = await get_session(request)
+    sid = session.get('sid')
+    await _shutdown(machine, sid)
+
+    await poll_for_disconnect(
+        machine.sid,
+        lambda: _wake(machine.sid, wol_capable_machine),
+        5,
+        60
+    )
+
+
+async def poll_for_disconnect(sid, callback, interval_in_s, max_time_in_s):
+    for i in range(0, max_time_in_s, interval_in_s):
+        await asyncio.sleep(interval_in_s)
+        if get_active_machine(sid) is None:
+            callback()
+            return
+    callback()
 
 
 async def shutdown_callback(status, error=None, sid=None):
