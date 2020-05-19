60124fd2ed7cca01135282544500f65a750e2adc
Robin Siep <RobinSiep1@gmail.com>
2020-05-19 16:07:41 +0200

    list port handler

---
+++ lighthouse/handlers/machine/port.py
@@ -0,0 +1,27 @@
+from aiohttp.web import json_response
+from aiohttp_session import get_session
+
+from lighthouse.app import sio
+from lighthouse.handlers.machine import get_machine
+from lighthouse.lib.exceptions import JsonHTTPConflict
+from lighthouse.lib.routes import routes
+from lighthouse.lib.security.decorators import permission_required
+from lighthouse.machine import get_active_machine
+
+
+@routes.get("/machines/{id}/ports")
+@permission_required('ports')
+async def list_ports(request):
+    machine = get_machine(request)
+
+    if get_active_machine(machine.sid) is None:
+        raise JsonHTTPConflict("The machine is not online")
+
+    session = await get_session(request)
+    sid = session.get('sid')
+
+    def ports_callback(ports):
+        print(ports)
+
+    await sio.emit('emit_ports', to=machine.sid, callback=ports_callback)
+    return json_response()
--- lighthouse/lib/security/__init__.py
+++ lighthouse/lib/security/__init__.py
@@ -20,7 +20,7 @@ class LighthouseIdentityPolicy(SessionIdentityPolicy):
 
 class DefaultAuthorizationPolicy(AbstractAuthorizationPolicy):
     user_permissions = ('connect', 'disconnect', 'wake_on_lan', 'shutdown',
-                        'reboot')
+                        'reboot', 'ports')
     oauth_permissions = ('connect', 'disconnect', 'identify', 'sys_info')
 
     def __init__(self, user_identity, *args, **kwargs):
